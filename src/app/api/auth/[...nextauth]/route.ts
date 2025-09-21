981798531b74f4e6e84392f47f95c5685ad69fa1
JFConstancia <jef.constancia@gmail.com>
2025-09-21 11:30:29 +0200

    build fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../../../prisma/client";
-import bcrypt from "bcrypt";
+import NextAuth from "next-auth"
+import { authOptions } from "../../../../lib/auth"
 
-const googleProvider = GoogleProvider({
-  clientId: process.env.GOOGLE_CLIENT_ID!,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-})
-
-const credentialsProvider = CredentialsProvider({
-  name: "Credentials",
-  credentials: {
-    email: { label: "Email", type: "email", placeholder: "Email" },
-    password: { label: "Password", type: "password" }
-  },
-  async authorize(credentials, req) {
-    if(!credentials?.email || !credentials?.password) return null
-
-    const user = await prisma.user.findUnique({ where: { email: credentials.email } })
-    if(!user) return null
-
-    const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword!)
-    return passwordsMatch ? user : null
-  }
-})
-
-export const authOptions: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET!,
-  session: { strategy: "jwt" },
-  providers: [googleProvider, credentialsProvider],
-  adapter: PrismaAdapter(prisma),
-}
-
-const handler = NextAuth({ ...authOptions })
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,10 @@
 // app/dashboard/layout.tsx (server component)
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
-  if (!session) redirect("/api/auth/signin"); // of "/login"
+  if (!session) redirect("/api/auth/signin");
   return <>{children}</>;
 }
